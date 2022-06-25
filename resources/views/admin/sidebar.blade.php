1053426d8a2e71dbb3b911c28ddc561d396b4f75
beko_002 <beksulton1112@gmail.com>
2022-06-26 00:20:54 +0500

    admin panel .....

---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,22 @@
+<div class="sidebar sidebar-style-2">
+    <div class="sidebar-wrapper scrollbar scrollbar-inner">
+        <div class="sidebar-content">
+            <ul class="nav nav-primary">
+                <li class="nav-item {{  request()->routeIs('admin.users.index') ? 'active' : '' }}">
+                    <a href="#">
+                        <i class="fas fa-user"></i>
+                        <p>Пользователь</p>
+                    </a>
+                </li>
+                <li class="nav-item {{ request()->routeIs('admin.posts.index') ? 'active' : '' }} ">
+                    <a href="#">
+                        <i class="fas fa-pen-square"></i>
+                        <p>Новости</p>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::prefix('admin')->name('admin.')->middleware(['web', 'auth'])->group(function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('users', App\Http\Controllers\UserController::class);
+});
